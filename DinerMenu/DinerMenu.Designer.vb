abcc5d6c165181f6f12125f8c759986bd3552cce
Jason Permann <JaceFoxman@gmail.com>
2025-03-22 16:57:37 -0600

    Add Buttons and Labels

--- DinerMenu/DinerMenu.Designer.vb
+++ DinerMenu/DinerMenu.Designer.vb
@@ -22,17 +22,99 @@ Partial Class DinerMenu
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.DinerNameLabel = New System.Windows.Forms.Label()
+        Me.DisplaySpecialLabel = New System.Windows.Forms.Label()
+        Me.SoupButton = New System.Windows.Forms.Button()
+        Me.Saladbutton = New System.Windows.Forms.Button()
+        Me.FishButton = New System.Windows.Forms.Button()
+        Me.ExitButton = New System.Windows.Forms.Button()
         Me.SuspendLayout()
+        '
+        'DinerNameLabel
+        '
+        Me.DinerNameLabel.AutoSize = True
+        Me.DinerNameLabel.Font = New System.Drawing.Font("Script MT Bold", 28.0!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Underline), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.DinerNameLabel.Location = New System.Drawing.Point(207, 20)
+        Me.DinerNameLabel.Name = "DinerNameLabel"
+        Me.DinerNameLabel.Size = New System.Drawing.Size(360, 67)
+        Me.DinerNameLabel.TabIndex = 0
+        Me.DinerNameLabel.Text = "Hogan's Diner"
+        '
+        'DisplaySpecialLabel
+        '
+        Me.DisplaySpecialLabel.Font = New System.Drawing.Font("Constantia", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.DisplaySpecialLabel.Location = New System.Drawing.Point(12, 87)
+        Me.DisplaySpecialLabel.Name = "DisplaySpecialLabel"
+        Me.DisplaySpecialLabel.Size = New System.Drawing.Size(776, 232)
+        Me.DisplaySpecialLabel.TabIndex = 1
+        '
+        'SoupButton
+        '
+        Me.SoupButton.BackColor = System.Drawing.Color.PaleGoldenrod
+        Me.SoupButton.Font = New System.Drawing.Font("Magneto", 16.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.SoupButton.Location = New System.Drawing.Point(44, 322)
+        Me.SoupButton.Name = "SoupButton"
+        Me.SoupButton.Size = New System.Drawing.Size(159, 116)
+        Me.SoupButton.TabIndex = 2
+        Me.SoupButton.Text = "Soup"
+        Me.SoupButton.UseVisualStyleBackColor = False
+        '
+        'Saladbutton
+        '
+        Me.Saladbutton.BackColor = System.Drawing.Color.SeaGreen
+        Me.Saladbutton.Font = New System.Drawing.Font("Magneto", 16.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Saladbutton.Location = New System.Drawing.Point(230, 322)
+        Me.Saladbutton.Name = "Saladbutton"
+        Me.Saladbutton.Size = New System.Drawing.Size(159, 116)
+        Me.Saladbutton.TabIndex = 3
+        Me.Saladbutton.Text = "Salad"
+        Me.Saladbutton.UseVisualStyleBackColor = False
+        '
+        'FishButton
+        '
+        Me.FishButton.BackColor = System.Drawing.Color.LightSkyBlue
+        Me.FishButton.Font = New System.Drawing.Font("Magneto", 16.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.FishButton.Location = New System.Drawing.Point(418, 322)
+        Me.FishButton.Name = "FishButton"
+        Me.FishButton.Size = New System.Drawing.Size(159, 116)
+        Me.FishButton.TabIndex = 4
+        Me.FishButton.Text = "Fish"
+        Me.FishButton.UseVisualStyleBackColor = False
+        '
+        'ExitButton
+        '
+        Me.ExitButton.BackColor = System.Drawing.SystemColors.ActiveBorder
+        Me.ExitButton.Font = New System.Drawing.Font("Magneto", 16.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ExitButton.Location = New System.Drawing.Point(604, 322)
+        Me.ExitButton.Name = "ExitButton"
+        Me.ExitButton.Size = New System.Drawing.Size(159, 116)
+        Me.ExitButton.TabIndex = 5
+        Me.ExitButton.Text = "Exit"
+        Me.ExitButton.UseVisualStyleBackColor = False
         '
         'DinerMenu
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.ExitButton)
+        Me.Controls.Add(Me.FishButton)
+        Me.Controls.Add(Me.Saladbutton)
+        Me.Controls.Add(Me.SoupButton)
+        Me.Controls.Add(Me.DisplaySpecialLabel)
+        Me.Controls.Add(Me.DinerNameLabel)
         Me.Name = "DinerMenu"
-        Me.Text = "Form1"
+        Me.Text = "Diner Menu"
+        Me.TransparencyKey = System.Drawing.Color.Black
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
+    Friend WithEvents DinerNameLabel As Label
+    Friend WithEvents DisplaySpecialLabel As Label
+    Friend WithEvents SoupButton As Button
+    Friend WithEvents Saladbutton As Button
+    Friend WithEvents FishButton As Button
+    Friend WithEvents ExitButton As Button
 End Class
--- DinerMenu/DinerMenu.vb
+++ DinerMenu/DinerMenu.vb
@@ -1,4 +1,9 @@
 ﻿Public Class DinerMenu
+    'Jason Permann
+    'Spring 2025
+    'RCET2265
+    'Diner Menu
+    'https://github.com/JaceFoxman/DinerMenu.git
     Private Sub DinerMenu_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
     End Sub
